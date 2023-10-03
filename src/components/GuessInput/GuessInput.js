9e71d5c7af746d03f5f806c3cd52ebc7f0e3872d
Jason Hanggi <jason@tablexi.com>
2023-10-02 21:58:32 -0500

    Add ability to submit a guess (to console)

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = useState("");
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.info({ guess });
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(e) => setGuess(e.target.value.toLocaleUpperCase())}
+        required
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
